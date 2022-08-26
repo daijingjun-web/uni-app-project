1e2f376b29cde15f139666e6b77ffbf61a5ef1fe
daijingjun-web <1808442433@qq.com>
2022-08-26 10:27:36 +0800

    提交main到本地仓库

---
+++ unpackage/dist/dev/mp-weixin/package1/goods_detail/goods_detail.js
@@ -0,0 +1,33 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["package1/goods_detail/goods_detail"],{
+
+/***/ 42:
+/*!******************************************************************************************************!*\
+  !*** C:/Users/代景军/Desktop/uni-app-project/main.js?{"page":"package1%2Fgoods_detail%2Fgoods_detail"} ***!
+  \******************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 4));
+var _goods_detail = _interopRequireDefault(__webpack_require__(/*! ./package1/goods_detail/goods_detail.vue */ 43));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;createPage(_goods_detail.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 43:
+/*!***********************************************************************************!*\
+  !*** C:/Users/代景军/Desktop/uni-app-project/package1/goods_detail/goods_detail.vue ***!
+  \***********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\代景军\\Desktop\\uni-app-project\\package1\\goods_detail\\goods_detail.vue'");
+
+/***/ })
+
+},[[42,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/package1/goods_detail/goods_detail.js.map
---
+++ unpackage/dist/dev/mp-weixin/package1/goods_list/goods_list.js
@@ -0,0 +1,33 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["package1/goods_list/goods_list"],{
+
+/***/ 50:
+/*!**************************************************************************************************!*\
+  !*** C:/Users/代景军/Desktop/uni-app-project/main.js?{"page":"package1%2Fgoods_list%2Fgoods_list"} ***!
+  \**************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 4));
+var _goods_list = _interopRequireDefault(__webpack_require__(/*! ./package1/goods_list/goods_list.vue */ 51));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;createPage(_goods_list.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 51:
+/*!*******************************************************************************!*\
+  !*** C:/Users/代景军/Desktop/uni-app-project/package1/goods_list/goods_list.vue ***!
+  \*******************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\代景军\\Desktop\\uni-app-project\\package1\\goods_list\\goods_list.vue'");
+
+/***/ })
+
+},[[50,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/package1/goods_list/goods_list.js.map
